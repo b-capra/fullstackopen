2a553227e2424cf8fe9d8be0febde577428d6423
b-capra <caprabmc@gmail.com>
2022-11-22 15:17:56 -0500

    Fix button component

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,26 +2,25 @@ import { useState } from 'react'
 
 const Heading = ({text}) => <h1>{text}</h1>
 
-const Button = ({text}) => {
-  return (
-    <button>{text}</button>
-  )
-}
+const Button = ({text, onClick}) => <button onClick={onClick}>{text}</button>
 
 const Total = ({text, total}) => <div>{text}: {total}</div>
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const addGood = () => setGood(good + 1)
+  const addNeutral = () => setNeutral(neutral + 1)
+  const addBad = () => setBad(bad + 1)
+
   return (
     <div>
       <Heading text='Give Feedback' />
-      <Button text='Good'/>
-      <Button text='Neutral'/>
-      <Button text='Bad'/>
+      <Button text='Good' onClick={addGood} />
+      <Button text='Neutral' onClick={addNeutral} />
+      <Button text='Bad' onClick={addBad} />
       <Heading text='Statistics' />
       <Total text='Good' total={good} />
       <Total text='Neutral' total={neutral} />
