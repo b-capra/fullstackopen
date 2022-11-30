c48c1debb5a63d0081f76de53dd555c540889b0a
b-capra <caprabmc@gmail.com>
2022-11-30 15:59:07 -0500

    Shorten reduce function

--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -2,7 +2,7 @@ const Total = ({parts}) => {
   return (
     <p>
       <b>Number of exercises: </b> 
-      {parts.reduce(function (acc, part) {return acc + part.exercises}, 0)}
+      {parts.reduce((acc, part) => acc + part.exercises, 0)}
     </p>
   )
 }
